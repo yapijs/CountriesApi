90372fc8bde72dd6a1d5d0a09603b3e1bb72466c
yapijs <daviszarins90@gmail.com>
2022-12-03 20:50:20 +0200

    added error handling

--- src/main/java/io/exercise/countriesapi/CountriesService.java
+++ src/main/java/io/exercise/countriesapi/CountriesService.java
@@ -2,11 +2,14 @@ package io.exercise.countriesapi;
 
 import io.exercise.countriesapi.domain.Country;
 import io.exercise.countriesapi.dto.CountryList;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -17,7 +20,9 @@ public class CountriesService {
     }
 
     public List<String> getNeighbours(String countryCode) {
-        return getNeighboursFromCountry(getExternalCountryInfo(countryCode));
+        return getNeighboursFromCountry(
+                getExternalCountryInfo(countryCode)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong country code provided")));
     }
 
     private List<String> getNeighboursFromCountry(Country country) {
@@ -27,22 +32,36 @@ public class CountriesService {
                 .toList();
     }
 
-    private Country getExternalCountryInfo(String countryCode) {
+    private Optional<Country> getExternalCountryInfo(String countryCode) {
         return WebClient
-                .create(EXTERNAL_COUNTRY_API_URL + countryCode)
-                .get()
-                .retrieve()
-                .bodyToMono(Country.class)
-                .block();
+                    .create(EXTERNAL_COUNTRY_API_URL + countryCode)
+                    .get()
+                    .retrieve()
+                    .bodyToMono(Country.class)
+                    .onErrorComplete()
+                    .blockOptional();
     }
 
     public CountryList[] getListNeighboursMultipleCountries(List<String> countryCodeList) {
-        List<Country> countryObjectList = new ArrayList<>();
-        for (String countryCode: countryCodeList) {
-            countryObjectList.add(getExternalCountryInfo(countryCode));
+        List<Country> countryObjectList = getListOfCountries(countryCodeList);
+
+        if (countryObjectList.size() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+
         return countryObjectList.stream()
                 .map(country -> new CountryList(country.getOfficialName(), getNeighboursFromCountry(country)))
                 .toArray(CountryList[]::new);
+    }
+
+    private List<Country> getListOfCountries(List<String> countryCodeList) {
+        List<Country> countryList = new ArrayList<>();
+
+        countryCodeList.stream()
+                .map(this::getExternalCountryInfo)
+                .forEach(country -> country
+                        .ifPresent(countryList::add));
+
+        return countryList;
     }
 }
